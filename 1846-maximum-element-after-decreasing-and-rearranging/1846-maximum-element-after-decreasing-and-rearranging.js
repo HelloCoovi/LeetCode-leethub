0738a508328f44dc9b64117a83800a65ee2bab6d
Coovi <85853962+jangg0627@users.noreply.github.com>
2023-11-15 16:54:08 +0900

    Time: 76 ms (38.10%), Space: 52.2 MB (33.33%) - LeetHub

---
+++ 1846-maximum-element-after-decreasing-and-rearranging/1846-maximum-element-after-decreasing-and-rearranging.js
@@ -0,0 +1,15 @@
+/**
+ * @param {number[]} arr
+ * @return {number}
+ */
+var maximumElementAfterDecrementingAndRearranging = function(arr) {
+    let result = 0
+    
+    arr.sort((a, b) => a - b).forEach((num, idx) => {
+        if (num > result) {
+            result += 1
+        }
+    })
+    
+    return result
+};
